ed24118f649222a05a72fb73c2ddb2724e3f5a07
Sudarshan Anuse <anusesudarshan11@gmail.com>
2023-01-26 15:24:57 +0530

    Student data add

--- Demo/src/com/Test.java
+++ Demo/src/com/Test.java
@@ -3,5 +3,11 @@ package com;
 public class Test {
 public static void main(String[] args) {
 	System.out.println(".......Check Feature Create Class Branch..........");
+	
+	Student s = new Student();
+	s.setId(89);
+	s.setName("Sudarshan");
+	s.setAddress("Solapur");
+	System.out.println("Student Data"+s);
 }
 }
